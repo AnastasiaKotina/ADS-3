ae278ebfbbad49be57ae6d23d528a72eeb29a668
AnastasiaKotina <160050075+AnastasiaKotina@users.noreply.github.com>
2024-03-18 22:19:43 +0300

    ADS 3upd.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@ int cbinsearch(int* arr, int size, int value) {
 	int left = 0;
 	int right = size - 1;
     
-    while (left <= right) {
+	while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] == value) {
             count++;
